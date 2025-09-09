7df2cd8ebf5211dd073be7b15c64192bba605f02
Sergey O. <onsergey@mail.ru>
2025-09-09 17:26:10 +0000

    Update InstallHR

--- InstallHR.php
+++ InstallHR.php
@@ -1,5 +1,4 @@
 <?php
-
 $_SERVER['DOCUMENT_ROOT'] = '/home/bitrix/www';
 $DOCUMENT_ROOT = $_SERVER['DOCUMENT_ROOT'];
 
@@ -15,8 +14,20 @@ define("NO_AGENT_CHECK", true);
 @ignore_user_abort(true);
 
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
-
 ///////////////////////////////////// from this place you may insert code to PHP command tool in Bitrix admin panel
+global $DB;
+
+//Step 1: Delete HR module from tables
+$zapr = "delete FROM 'b_module_to_module' where TO_CLASS like '%uman%' and TO_CLASS like '%esources%'";
+$result = $DB->query($zapr);
+
+$zapr = "delete FROM 'b_module' where ID like '%uman%' and ID like '%esources%'";
+$result = $DB->query($zapr);
+
+$zapr = "delete FROM 'b_agent' where NAME like '%uman%' and NAME like '%esources%'";
+$result = $DB->query($zapr);
+
+//Step 2: Install HR module from tables
 include $_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/humanresources/install/index.php";
 $a=new HumanResources();
 $a->doInstall();
